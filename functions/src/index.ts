f9bc96669cb116fc3eb2236b3ad53962c140724e
Max Rozdobudko <max.rozdobudko@gmail.com>
2021-06-04 11:39:44 +0300

    Stale changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as processing from 'modules/processing'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
+});
+
+export const image = functions.https.onRequest((req, res) => {
+    if (req.method == "POST") {
+        return
+    }
 });
